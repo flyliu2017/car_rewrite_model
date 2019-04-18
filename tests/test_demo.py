ec0c1c0455aaefac5da57592d696fd8e6c26acbe
ludezheng <ludezheng@aidigger.com>
2019-04-18 15:07:10 +0800

    Update test_demo.py

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -12,7 +12,7 @@ class TestDemo(unittest.TestCase):
         vocab_filepath = 'oss://modelzoo/dev/ludezheng/vocab_jieba.txt'
         keywords_filepath = 'oss://modelzoo/dev/ludezheng/keywords_table.txt'
         car_info_filepath = 'oss://modelzoo/dev/ludezheng/car_info.txt'
-        stop_words_filepath = 'oss://modelzoo/dev/ludezheng/sotp_words.txt'
+        stop_words_filepath = 'oss://modelzoo/dev/ludezheng/stop_words.txt'
         m = CarRewriteBaseKeywords(total_comments_filepath=total_comments_filepath, vocab_filepath=vocab_filepath, keywords_filepath=keywords_filepath, car_info_filepath=car_info_filepath, stop_words_filepath=stop_words_filepath)
         test_data = [{'id': 1, 'content': '驾驶性能超好，还真别说，会开车的，开着瑞虎7操控感觉肯定也不错，外观造型漂亮，设计不会老土，最后就是虽然加速动力欠缺，但是确实很省油', 'domain': '最满意'}]
         ret = m.predict(test_data)
